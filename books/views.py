4fb3596cdee6eb072e612649971b2c56c2fde61c
Erik Agayan <erik.agayan1@gmail.com>
2023-08-14 15:51:52 +0300

    Add serializers, BookViewSet and books.urls.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from books.models import Book
+from rest_framework import mixins
+from books.serializers import BookSerializer, BookListSerializer, BookDetailSerializer
+
+
+class BookViewSet(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  GenericViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+
+        if self.action == "retrieve":
+            return BookDetailSerializer
+
+        return BookSerializer
