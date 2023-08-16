5dbc29750fa971ae2290e663f5e8d568f7e7a1fe
Rostyslav <rostyslav.furman@gmail.com>
2023-08-16 16:21:25 +0300

    telegram_interface_for_clients

--- telegram_bot/commands.py
+++ telegram_bot/commands.py
@@ -1,4 +1,6 @@
 from datetime import date
+
+import requests
 from django.db.models import Sum
 from borrowings.models import Borrowing
 from books.models import Book
@@ -6,6 +8,7 @@ from telegram_bot import messages
 from aiogram import types
 from telegram_bot.apps import dp
 from telegram_bot.apps import bot
+from library_config.settings import JWT_TOKEN
 
 
 @dp.message_handler(commands=["help"])
@@ -68,3 +71,60 @@ async def count_titles(message: types.Message):
         message.from_user.id,
         f"number of titles: {count_number_of_titles}"
     )
+
+user_response = ""
+
+
+def recive_data(user_response):
+    response = requests.get(
+        "http://localhost:8000/api/borrowings/",
+        headers={"Authorization": f"Bearer {JWT_TOKEN}"},
+    )
+
+    data = response.json()
+    user_borrowed_books = []
+    user_email = user_response
+    for book in data:
+        if book["user"] == user_email:
+            user_borrowed_books.append(
+                {
+                    "id": book["id"],
+                    "borrow_date": book["borrow_date"],
+                    "expected_return_date": book["expected_return_date"],
+                    "actual_return_date": book["actual_return_date"],
+                    "book": book["book"],
+                }
+            )
+    reply_text = ""
+    if user_borrowed_books:
+        reply_text += f"Borrowed books for {user_email}:\n"
+
+        for book in user_borrowed_books:
+            reply_text += (
+                f"Book: {book['book']}\n"
+                f"Borrow Date: {book['borrow_date']}\n"
+                f"Expected Return Date: {book['expected_return_date']}\n"
+                f"Actual Return Date: {book['actual_return_date']}\n\n"
+            )
+        return reply_text
+    else:
+        return f"No borrowed books found for {user_email}"
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.reply("The bot is ready to interact with you.")
+    await message.reply("Please enter your mailing address:")
+
+
+@dp.message_handler(commands=["get_list_borrowing"])
+async def get_data_from_drf(message: types.Message):
+    # Process the data and send a response
+    await message.reply(recive_data(user_response))
+
+
+@dp.message_handler(lambda message: True)
+async def handle_text(message: types.Message):
+    global user_response
+    user_response = message.text.strip()
+    await message.reply(recive_data(user_response))
